a00b769dfaf1028258464a580e286400c3aba70b
Ahmed <ahmed.mabohamer200@gmail.com>
2025-09-05 18:03:57 +0300

    make the banner hidden in login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
 import Banner from "./components/Banner";
 import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const { pathname } = useLocation();
   useEffect(() => {
     document.title = "منصة الرياضيات";
   }, []);
   return (
     <div style={{ direction: "rtl", minHeight: "100vh" }}>
       <main className="container py-3">
-        <Banner />
+        {pathname !== "/login" && <Banner />}
         <AppRoutes />
       </main>
     </div>
