05674e1a4db9336da92308e5703e73162bd9d5e5
Ahmed <ahmed.mabohamer200@gmail.com>
2025-09-05 20:37:34 +0300

    feat: Reposition logout button under school logo in Banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -15,12 +15,6 @@ export default function Banner() {
 
   return (
     <div className="py-3 brand-banner position-relative">
-      <button
-        className="btn btn-outline-light position-absolute top-0 start-0 m-2"
-        onClick={handleLogout}
-      >
-        تسجيل الخروج
-      </button>
       <div className="row g-3 align-items-center justify-content-center text-center px-2 py-2">
         <div className="col-12 col-md-4 d-flex justify-content-center">
           {ministryLogo && (
@@ -37,17 +31,20 @@ export default function Banner() {
         <div className="col-12 col-md-4 d-flex align-items-center justify-content-center">
           <h2 className="m-0 text-white">منصة الرياضيات</h2>
         </div>
-        <div className="col-12 col-md-4 d-flex justify-content-center">
+        <div className="col-12 col-md-4 d-flex flex-column align-items-center justify-content-center">
           {schoolLogo && (
             <img
               src={schoolLogo}
               alt="اسم المدرسة"
-              className="img-fluid brand-logo"
+              className="img-fluid brand-logo mb-2"
               onError={(e) => {
                 e.currentTarget.style.display = "none";
               }}
             />
           )}
+          <button className="btn btn-outline-light" onClick={handleLogout}>
+            تسجيل الخروج
+          </button>
         </div>
       </div>
     </div>
