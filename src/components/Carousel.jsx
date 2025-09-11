41051aa2f809bb26fbf0c1cb509b6d8bd2d6694d
Ahmed Marwan <73545982+A7medMarwan@users.noreply.github.com>
2025-09-11 17:51:56 +0300

    Update Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -39,7 +39,7 @@ const Carousel = ({ items }) => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
